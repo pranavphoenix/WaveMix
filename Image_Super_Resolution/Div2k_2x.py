2c7be2f618bca3f7d60231f12be1b75857448fa9
Cloudwalker <pranav13phoenix@gmail.com>
2023-03-04 01:02:30 +0530

    Update Div2k_2x.py

--- Image_Super_Resolution/Div2k_2x.py
+++ Image_Super_Resolution/Div2k_2x.py
@@ -23,11 +23,19 @@ import torchmetrics
 from datasets import load_dataset
 from torchmetrics import StructuralSimilarityIndexMeasure, PeakSignalNoiseRatio
 
+from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+
+
+
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.get_device_properties(device))
 
 dataset_train = load_dataset('eugenesiow/Div2k', 'bicubic_x2', split='train', cache_dir = '/workspace/')
 dataset_val = load_dataset('eugenesiow/Div2k', 'bicubic_x2', split='validation', cache_dir = '/workspace/')
+# dataset_val = load_dataset('eugenesiow/Set5', 'bicubic_x2', split='validation', cache_dir = '/workspace/')
+# dataset_val = load_dataset('eugenesiow/BSD100', 'bicubic_x2', split='validation', cache_dir = '/workspace/')
+# dataset_val = load_dataset('eugenesiow/Urban100', 'bicubic_x2', split='validation', cache_dir = '/workspace/')
 
 class SuperResolutionDataset(Dataset):
     def __init__(self, dataset, transform_img=None, transform_target=None):
@@ -56,15 +64,15 @@ class SuperResolutionDataset(Dataset):
 
 
 transform_img = transforms.Compose(
-        [transforms.Resize([256,256]),
+        [transforms.Resize([512,512],interpolation = transforms.InterpolationMode.BICUBIC),
             transforms.ToTensor(),
-            transforms.Normalize((0.4485, 0.4375, 0.4046), (0.2698, 0.2557, 0.2802))
+            # transforms.Normalize((0.4485, 0.4375, 0.4046), (0.2698, 0.2557, 0.2802))
      ])
 
 transform_target = transforms.Compose(
         [transforms.Resize([512,512]),
             transforms.ToTensor(),
-            transforms.Normalize((0.4485, 0.4375, 0.4046), (0.2698, 0.2557, 0.2802))
+            # transforms.Normalize((0.4485, 0.4375, 0.4046), (0.2698, 0.2557, 0.2802))
      ])
 
 trainset = SuperResolutionDataset(dataset_train, transform_img, transform_target)
@@ -431,8 +439,7 @@ class Waveblock(nn.Module):
         dropout = 0.5,
     ):
         super().__init__()
-        
-      
+              
         self.feedforward1 = nn.Sequential(
                 nn.Conv2d(final_dim, final_dim*mult,1),
                 nn.GELU(),
@@ -443,30 +450,21 @@ class Waveblock(nn.Module):
             
             )
         
-
         self.reduction = nn.Conv2d(final_dim, int(final_dim/4), 1)
-        
-        
+                
     def forward(self, x):
         b, c, h, w = x.shape
         
         x = self.reduction(x)
         
-
         Y1, Yh = xf1(x)
-        
-        
+                
         x1 = torch.reshape(Yh[0], (b, int(c*3/4), int(h/2), int(w/2)))
-       
-        
+               
         x1 = torch.cat((Y1,x1), dim = 1)
-        
-       
-        
+                
         x1 = self.feedforward1(x1)
-        
-        
-        
+                
         return x1
 
 class WaveMix(nn.Module):
@@ -486,8 +484,8 @@ class WaveMix(nn.Module):
             self.layers.append(Waveblock(mult = mult, ff_channel = ff_channel, final_dim = final_dim, dropout = dropout))
         
         self.expand = nn.Sequential(
-            nn.ConvTranspose2d(final_dim,int(final_dim/2), 4, stride=2, padding=1),
-            nn.Conv2d(int(final_dim/2), 3, 1)
+            # nn.ConvTranspose2d(final_dim,int(final_dim/2), 4, stride=2, padding=1),
+            nn.Conv2d(final_dim, 3, 1)
         )
 
         self.conv = nn.Sequential(
@@ -495,8 +493,7 @@ class WaveMix(nn.Module):
             nn.Conv2d(int(final_dim/2), final_dim, 3, 1, 1)
         )
 
-      
-
+        
     def forward(self, img):
         x = self.conv(img)  
         
@@ -509,19 +506,18 @@ class WaveMix(nn.Module):
         return out
 
 model = WaveMix(
-    depth = 8,
+    depth = 4,
     mult = 2,
     ff_channel = 128,
     final_dim = 128,
     dropout = 0.5
 )
 model.to(device)
-print(summary(model, (3,256,256)))
+print(summary(model, (3,512,512)))
 
 scaler = torch.cuda.amp.GradScaler()
 
-batch_size = 32
-
+batch_size = 16
 
 testloader = torch.utils.data.DataLoader(valset, batch_size=batch_size*2,
                                          shuffle=False, num_workers=2, pin_memory=True, prefetch_factor=2, persistent_workers=2)
@@ -529,12 +525,14 @@ testloader = torch.utils.data.DataLoader(valset, batch_size=batch_size*2,
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=2, pin_memory=True, prefetch_factor=2, persistent_workers=2)
 
-ssim = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
 psnr = PeakSignalNoiseRatio().to(device)
+ssim = StructuralSimilarityIndexMeasure(data_range=1.0).to(device)
 
+ssim_module = SSIM(data_range=255, size_average=True, channel=3)
 
-
-criterion = nn.MSELoss()
+criterion1 =  nn.L1Loss() 
+criterion2 =  nn.MSELoss() 
+lamda = 0.5
 scaler = torch.cuda.amp.GradScaler()
 top1 = []
 traintime = []
@@ -547,6 +545,7 @@ while counter < 25:
     t0 = time.time()
     epoch_psnr = 0
     epoch_loss = 0
+    
     model.train()
 
     with tqdm(trainloader, unit="batch") as tepoch:
@@ -557,12 +556,12 @@ while counter < 25:
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
+            data_range = outputs.max().unsqueeze(0)
             with torch.cuda.amp.autocast():
-                loss = criterion(outputs, labels)
+                loss =  1 - ssim_module(outputs, labels) #+ criterion1(outputs, labels) + lamda*criterion2(outputs, labels) 
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
-
 
             epoch_PSNR = psnr(outputs, labels) 
             
@@ -577,14 +576,10 @@ while counter < 25:
         for data in testloader:
             images, labels = data[0].to(device), data[1].to(device)
             outputs = model(images)
-
-
             
             PSNR += psnr(outputs, labels) / len(testloader)
-
             
-        
-    print(f"Epoch : {epoch+1} -PSNR: {PSNR:.2f}-Test Time: {time.time() - t1:.0f}\n")
+    print(f"Epoch : {epoch+1} - PSNR: {PSNR:.2f}  - Test Time: {time.time() - t1:.0f}\n")
 
     top1.append(PSNR)
     traintime.append(t1 - t0)
@@ -598,16 +593,16 @@ while counter < 25:
         print(1)
         counter = 0
 
-
+PATH = 'superresolutionDiv2k2x.pth'
 model.load_state_dict(torch.load(PATH))
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9) #Second Optimizer
 
-
-for epoch in range(100):  # loop over the dataset multiple times
+for epoch in range(1):  # loop over the dataset multiple times
     t0 = time.time()
     epoch_psnr = 0
     epoch_loss = 0
     running_loss = 0.0
+    
     model.train()
 
     with tqdm(trainloader, unit="batch") as tepoch:
@@ -619,30 +614,26 @@ for epoch in range(100):  # loop over the dataset multiple times
           optimizer.zero_grad()
           outputs = model(inputs)
           with torch.cuda.amp.autocast():
-              loss = criterion(outputs, labels)
+              loss =  1 - ssim_module(outputs, labels) #+ criterion1(outputs, labels) + lamda*criterion2(outputs, labels) 
           scaler.scale(loss).backward()
           scaler.step(optimizer)
           scaler.update()
 
-
           epoch_PSNR = psnr(outputs, labels)
           epoch_loss += loss / len(trainloader)
           tepoch.set_postfix_str(f" loss : {epoch_loss:.4f} - PSNR: {epoch_PSNR:.4f}" )
-
     
     t1 = time.time()
     model.eval()
-    PSNR = 0
+    PSNR = 0   
     with torch.no_grad():
         for data in testloader:
             images, labels = data[0].to(device), data[1].to(device)
             outputs = model(images)
 
             PSNR += psnr(outputs, labels) / len(testloader)
-
-      
-        
-    print(f"Epoch : {epoch+1} -PSNR: {PSNR:.2f} - Test Time: {time.time() - t1:.0f}\n")
+     
+    print(f"Epoch : {epoch+1} - PSNR: {PSNR:.2f}  - Test Time: {time.time() - t1:.0f}\n")
 
     top1.append(PSNR)
     traintime.append(t1 - t0)
@@ -656,12 +647,12 @@ for epoch in range(100):  # loop over the dataset multiple times
 print('Finished Training')
 model.load_state_dict(torch.load(PATH))
 SSIM = 0
+
 with torch.no_grad():
         for data in testloader:
             images, labels = data[0].to(device), data[1].to(device)
             outputs = model(images)
 
             SSIM += float(ssim(outputs, labels)) / len(testloader)
-           
 
 print(f"Top PSNR : {max(top1):.2f} -Top SSIM : {SSIM:.4f} - Train Time: {min(traintime):.0f} -Test Time: {min(testtime):.0f}\n")

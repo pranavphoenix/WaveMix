458f963dcf717f39535f134450e0de7e68034edb
Cloudwalker <pranav13phoenix@gmail.com>
2023-10-17 00:35:29 +0530

    Create wavemix_73.py

---
+++ Saved_weights/ImageNet/wavemix_73.py
@@ -0,0 +1,20 @@
+import torch, wavemix
+from wavemix.classification import WaveMix
+
+model = WaveMix(
+    num_classes = 1000,
+    depth = 16,
+    mult = 2,
+    ff_channel = 256,
+    final_dim = 256,
+    dropout = 0.5,
+    level = 3,
+    initial_conv = 'pachify',
+    patch_size = 4
+
+)
+
+
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Saved_Models_Weights/ImageNet/imagenet_71.49.pth'
+
+model.load_state_dict(torch.hub.load_state_dict_from_url(url))

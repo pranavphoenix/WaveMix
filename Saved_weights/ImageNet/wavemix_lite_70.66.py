0273ad4e222eaf3b146508e94e176625f2d5aedb
Cloudwalker <pranav13phoenix@gmail.com>
2024-03-21 01:51:06 +0530

    Create wavemix_lite_70.66.py

---
+++ Saved_weights/ImageNet/wavemix_lite_70.66.py
@@ -0,0 +1,18 @@
+import torch, wavemix
+from wavemix.classification import WaveMix
+
+model = WaveMix(
+    num_classes = 1000,
+    depth = 16,
+    mult = 2,
+    ff_channel = 192,
+    final_dim = 192,
+    dropout = 0.5,
+    level = 1,
+    initial_conv = 'pachify',
+    patch_size = 4
+)
+
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Saved_Models_Weights/ImageNet/wavemix_192_16_level1_70.66.pth'
+
+model.load_state_dict(torch.hub.load_state_dict_from_url(url))

49d0e59e391da2230c1aef756abd71c5c9538d03
Cloudwalker <pranav13phoenix@gmail.com>
2024-04-22 06:42:40 +0530

    Create wavemix_28M_72.py

---
+++ Saved_weights/tinyimagenet/wavemix_28M_72.py
@@ -0,0 +1,19 @@
+import torch, wavemix
+from wavemix.classification import WaveMix
+
+model = WaveMix(
+    num_classes = 200,
+    depth = 16,
+    mult = 2,
+    ff_channel = 192,
+    final_dim = 192,
+    dropout = 0.5,
+    level = 3,
+    initial_conv = 'pachify',
+    patch_size = 4
+)
+
+
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Saved_Models_Weights/tinyimagenet/tiny_71.69.pth'
+
+model.load_state_dict(torch.hub.load_state_dict_from_url(url))

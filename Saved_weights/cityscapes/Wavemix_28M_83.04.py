feadea75d3ad34f2cbf18f1401666cf83fa0dbf3
Cloudwalker <pranav13phoenix@gmail.com>
2024-04-26 04:05:40 +0530

    Update Wavemix_28M_83.04.py

--- Saved_weights/cityscapes/Wavemix_28M_83.04.py
+++ Saved_weights/cityscapes/Wavemix_28M_83.04.py
@@ -19,6 +19,6 @@ model.pool = nn.Sequential(
             nn.Conv2d(64, 20, 1) 
 )
 
-url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Saved_Models_Weights/ImageNet/wavemix_192_16_75.06.pth'
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Saved_Models_Weights/cityscapes_83.04.pth'
 
 model.load_state_dict(torch.hub.load_state_dict_from_url(url))

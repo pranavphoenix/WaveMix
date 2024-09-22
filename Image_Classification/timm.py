6c35196749428db3774a25370b81c2ba0a99e196
Cloudwalker <pranav13phoenix@gmail.com>
2024-09-22 19:39:07 +0530

    Update timm.py

--- Image_Classification/timm.py
+++ Image_Classification/timm.py
@@ -1,60 +1,56 @@
-#Code to use in Timm Library
-#Replace the code in convmixer.py in pytorch-image-models/timm/models/ with this code and run
-
-""" ConvMixer
-
 """
+WaveMix models for image classification.
+Some implementations are modified from:
+timm (https://github.com/rwightman/pytorch-image-models),
+MambaOut (https://github.com/yuweihao/MambaOut/blob/main/models/mambaout.py)
+"""
+from functools import partial
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
-from torch.autograd import Function
-import pywt
-import functools
-from einops import rearrange, repeat
-from einops.layers.torch import Rearrange
-
-import pywt
-from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
-from .helpers import build_model_with_cfg, checkpoint_seq
-from .layers import SelectAdaptivePool2d
-
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from wavemix import Level1Waveblock, Level2Waveblock, Level3Waveblock, Level4Waveblock
+from einops.layers.torch import Rearrange
 
 def _cfg(url='', **kwargs):
     return {
         'url': url,
         'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
-        'crop_pct': .96, 'interpolation': 'bicubic',
+        'crop_pct': 1.0, 'interpolation': 'bicubic',
         'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD, 'classifier': 'head',
-        'first_conv': 'stem.0',
         **kwargs
     }
 
 
-device= torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-import wavemix
-from wavemix import Level4Waveblock, Level3Waveblock, Level2Waveblock, Level1Waveblock
-import torch.nn as nn
-from einops.layers.torch import Rearrange
+default_cfgs = {
+    'wavemix': _cfg(
+        url='https://huggingface.co/cloudwalker/wavemix/resolve/main/Saved_Models_Weights/ImageNet/wavemix_192_16_75.06.pth'),
+    'wavemix_lite': _cfg(
+        url='https://huggingface.co/cloudwalker/wavemix/resolve/main/Saved_Models_Weights/ImageNet/wavemixlite_71.18.pth'),
+   
+}
 
-
-class ConvMixer(nn.Module):
+class WaveMix(nn.Module):
     def __init__(
-            self, depth, mult = 2, ff_channel = 16, final_dim = 16, dropout = 0., level =3, patch_size = 4, in_chans=3, num_classes=1000, global_pool='avg',
-            act_layer=nn.GELU, **kwargs):
-        super().__init__()
-        self.num_classes = num_classes
-        self.num_features = final_dim
-        self.grad_checkpointing = False
+        self,
+        *,
+        num_classes=1000,
+        depth = 16,
+        mult = 2,
+        ff_channel = 192,
+        final_dim = 192,
+        dropout = 0.5,
+        level = 3,
+        initial_conv = 'pachify', # or 'strided'
+        patch_size = 4,
+        stride = 2,
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(3, int(final_dim/4),3, 1, 1),
-            nn.Conv2d(int(final_dim/4), int(final_dim/2), 3, 1, 1),
-            nn.Conv2d(int(final_dim/2), final_dim, patch_size, patch_size),
-            nn.GELU(),
-            nn.BatchNorm2d(final_dim)
-            )
+    ):
+        super().__init__()
+
+        self.num_classes = num_classes
         
         self.layers = nn.ModuleList([])
         for _ in range(depth): 
@@ -66,55 +62,94 @@ class ConvMixer(nn.Module):
                     self.layers.append(Level2Waveblock(mult = mult, ff_channel = ff_channel, final_dim = final_dim, dropout = dropout))
                 else:
                     self.layers.append(Level1Waveblock(mult = mult, ff_channel = ff_channel, final_dim = final_dim, dropout = dropout))
-
         
-        
-        self.pool = nn.Sequential(
+        self.head = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             Rearrange('... () () -> ...'),
             nn.Linear(final_dim, num_classes)
         )
-       
 
-    @torch.jit.ignore
-    def group_matcher(self, coarse=False):
-        matcher = dict(conv=r'^conv', layers=r'^layers\.(\d+)')
-        return matcher
-
-    @torch.jit.ignore
-    def set_grad_checkpointing(self, enable=True):
-        self.grad_checkpointing = enable
-
-    @torch.jit.ignore
-    def get_classifier(self):
-        return self.head
-
-          
-    def forward_features(self, x):
-        x = self.conv(x)
-        if self.grad_checkpointing and not torch.jit.is_scripting():
-            x = checkpoint_seq(self.layers, x)
+        if initial_conv == 'strided':
+            self.conv = nn.Sequential(
+            nn.Conv2d(3, int(final_dim/2), 3, stride, 1),
+            nn.Conv2d(int(final_dim/2), final_dim, 3, stride, 1)
+        )
         else:
-            for attn in self.layers:
-                x = attn(x) + x
-        return x
+            self.conv = nn.Sequential(
+            nn.Conv2d(3, int(final_dim/4),3, 1, 1),
+            nn.Conv2d(int(final_dim/4), int(final_dim/2), 3, 1, 1),
+            nn.Conv2d(int(final_dim/2), final_dim, patch_size, patch_size),
+            nn.GELU(),
+            nn.BatchNorm2d(final_dim)
+            )
+        
+    
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
 
-    def forward_head(self, x, pre_logits: bool = False):
-        x = self.pool(x)
-        return x 
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'norm'}
+
+    def forward_features(self, x):
+        x = self.conv(x)   
+        for attn in self.layers:
+            x = attn(x) + x
+        return x
 
     def forward(self, x):
         x = self.forward_features(x)
-        x = self.forward_head(x)
+        x = self.head(x)
         return x
+    
 
 
-def _create_convmixer(variant, pretrained=False, **kwargs):
-    return build_model_with_cfg(ConvMixer, variant, pretrained, **kwargs)
+
+
+###############################################################################
+# a series of WaveMix models
+@register_model
+def wavemix(pretrained=False, **kwargs):
+    model =  WaveMix(
+        num_classes = 1000,
+        depth = 16,
+        mult = 2,
+        ff_channel = 192,
+        final_dim = 192,
+        dropout = 0.5,
+        level = 3,
+        initial_conv = 'pachify',
+        patch_size = 4)
+    model.default_cfg = default_cfgs['wavemix']
+    if pretrained:
+        state_dict = torch.hub.load_state_dict_from_url(
+            url= model.default_cfg['url'], map_location="cpu", check_hash=True)
+        model.load_state_dict(state_dict)
+    return model
+
 
 
 @register_model
-def convmixer(pretrained=False, **kwargs):
-    model_args = dict(depth = 16, mult = 2, ff_channel = 192, final_dim = 192, dropout = 0.5, level = 3, patch_size = 4, **kwargs)
-    return _create_convmixer('convmixer', pretrained, **model_args)
+def wavemix_lite(pretrained=False, **kwargs):
+    model = WaveMix(
+    num_classes = 1000,
+    depth = 16,
+    mult = 2,
+    ff_channel = 192,
+    final_dim = 192,
+    dropout = 0.5,
+    level = 1,
+    initial_conv = 'pachify',
+    patch_size = 4,
+        **kwargs)
+    model.default_cfg = default_cfgs['wavemix_lite']
+    if pretrained:
+        state_dict = torch.hub.load_state_dict_from_url(
+            url= model.default_cfg['url'], map_location="cpu", check_hash=True)
+        model.load_state_dict(state_dict)
+    return model
+
 
